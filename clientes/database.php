c4c9eea6204fe85a64ab8b537523801589407dbe
ricardomagalhaespita <ricardomagalhaespita@gmail.com>
2022-11-18 11:59:28 -0300

    First Commit

--- clientes/database.php
+++ clientes/database.php
@@ -1,4 +1,10 @@
 <?php 
+// Cabeçalho para receber API
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: PUT, POST, GET, DELETE, OPTIONS");
+header("Content-Type: application/json");
+header("Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Content-Type, Acess-Control-Allow-Methods, Authorization");
 
 	$hostname = 'localhost';
 	$username = 'root';
